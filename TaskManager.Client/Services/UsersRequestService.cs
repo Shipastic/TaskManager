50a4f3bf2026c5ae288835992e0034c8ba6d56c3
Shipastic <denkul87@gmail.com>
2023-10-12 23:13:16 +0300

    Addes service UserRequestService and tests of proj for userService

---
+++ TaskManager.Client/Services/UsersRequestService.cs
@@ -0,0 +1,115 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Client.Models;
+using TaskManager.Common.Models;
+
+namespace TaskManager.Client.Services
+{
+    public class UsersRequestService
+    {
+        private const string HOST = "http://localhost:40464/api/";
+
+        private string _userController = HOST + "users";
+
+        private string GetDataByUrl(string url, string userName = null, string password = null)
+        {
+            string result = string.Empty;
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+            request.Method = "POST";
+
+            if (userName != null && password != null)
+            {
+                string encoded = Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(userName + ":" + password));
+                request.Headers.Add("Authorization", "Basic " + encoded);
+            }
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+            {
+                string responseStr = reader.ReadToEnd();
+                result = responseStr;
+            }
+            return result;
+        }
+
+        private HttpStatusCode SendDataByUrl(HttpMethod method, string url, AuthToken token, string data)
+        {
+            HttpResponseMessage result = new();
+            HttpClient client = new();
+
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token.access_token);
+
+            var content = new StringContent(data, Encoding.UTF8, "application/json");
+
+            if (method == HttpMethod.Post)
+            {
+                result = client.PostAsync(url, content).Result;
+            }
+            if (method == HttpMethod.Patch)
+            {
+                result = client.PatchAsync(url, content).Result;
+            }
+
+            return result.StatusCode;
+        }
+
+        private HttpStatusCode DeleteDataByUrl(string url, AuthToken token)
+        {
+            HttpResponseMessage result = new();
+            HttpClient client = new();
+
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token.access_token);
+            result = client.DeleteAsync(url).Result;
+            return result.StatusCode;
+        }
+
+        public AuthToken GetToken(string userName, string password)
+        {
+            string url = HOST + "account/token";
+            string resultStr = GetDataByUrl(url, userName, password);
+            AuthToken token = JsonConvert.DeserializeObject<AuthToken>(resultStr);
+            return token;
+        }
+
+        public HttpStatusCode CreateUser(AuthToken token, UserModel user)
+        {
+            string userJson = JsonConvert.SerializeObject(user);
+            var result = SendDataByUrl(HttpMethod.Post, _userController, token, userJson);
+            return result;
+        }
+
+        public List<UserModel> GetAllUsers(AuthToken token)
+        {
+            string response = GetDataByUrl(_userController);
+            List<UserModel> users = JsonConvert.DeserializeObject<List<UserModel>>(response);
+            return users; 
+        }
+
+        public HttpStatusCode DeleteUser(AuthToken token, int userId)
+        {
+            var result = DeleteDataByUrl(_userController + $"/{userId}", token);
+            return result;
+        }
+
+        public HttpStatusCode CreateMultipleUsers(AuthToken token, List<UserModel> users)
+        {
+            string userJson = JsonConvert.SerializeObject(users);
+            var result = SendDataByUrl(HttpMethod.Post, _userController + "/all", token, userJson);
+            return result;
+        }
+
+        public HttpStatusCode UpdateUser(AuthToken token, UserModel user)
+        {
+            string userJson = JsonConvert.SerializeObject(user);
+            var result = SendDataByUrl(HttpMethod.Patch, _userController + $"/{user.Id}", token, userJson);
+            return result;
+        }
+    }
+}
